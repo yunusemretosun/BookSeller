e06661c675dd488c658840150459479fd2242326
yunus emre tosun <sn.emretosun@gmail.com>
2021-03-07 00:16:50 +0300

    commit on master

--- store/views.py
+++ store/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product,Category
+
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, 'store/home.html', {'products':products})
